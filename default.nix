c8875de69239e2b533ca13ae63b60035eae34cd2
Alexander Linder <seine@eloque.nz>
2024-08-11 14:04:53 +0200

    Update to v9.0.0

--- default.nix
+++ default.nix
@@ -5,6 +5,7 @@
 , autoPatchelfHook
 , makeWrapper
 , jdk
+, glibc
 , webkitgtk
 , gtk3
 , ... }:
@@ -20,16 +21,16 @@ let
     icon = ./src/share/icons/adoptium.png;
   };
 
-  libraryPath = lib.makeLibraryPath [ webkitgtk ];
+  libraryPath = lib.makeLibraryPath [ webkitgtk glibc ];
   binPath = lib.makeBinPath [ jdk ];
 
 in stdenv.mkDerivation rec {
   pname = "java-mission-control";
-  version = "8.3.0";
+  version = "9.0.0";
 
   src = fetchzip {
     url = "https://github.com/adoptium/jmc-build/releases/download/${version}/org.openjdk.jmc-${version}-linux.gtk.x86_64.tar.gz";
-    hash = "sha256-HOW5dvXGyqk5QHC+U5h7t9QFlioNylIu8idY1fIcZLI=";
+    hash = "sha256-zGeKdykSBIXbwwbklpcY4k3EVUh2uJxCZ2lWJrtY6lM=";
     stripRoot = false;
   };
 
@@ -44,7 +45,8 @@ in stdenv.mkDerivation rec {
     wrapProgram "$out/jmc" \
         --prefix LD_LIBRARY_PATH : "${libraryPath}" \
         --prefix PATH : "${binPath}" \
-        --prefix XDG_DATA_DIRS : "${gtk3}/share/gsettings-schemas/${gtk3.name}"
+        --prefix XDG_DATA_DIRS : "${gtk3}/share/gsettings-schemas/${gtk3.name}" \
+        --prefix PATH ':' "${jdk}/bin"
     ln -s $out/jmc $out/bin/java-mission-control
     ln -s ${desktopItem}/share/applications $out/share/applications
   '';  
